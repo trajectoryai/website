bffa6bce7d092444ddd8ea0c96e94ff988fad178
joshcarp <joshjcarp@gmail.com>
2024-07-04 14:48:00 -0400

    Update

---
+++ build.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+# Build the project.
+cd _source && hugo -t hugo-coder && cd ..
+
+rm -rf docs/*
+# Go To Public folder
+mv -f _source/public/* docs/
+
+# Add changes to git.
+git add -A
